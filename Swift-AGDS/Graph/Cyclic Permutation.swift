508a8a5cb96ffa0225ebd2b67a784e450bd8d2d1
yk-jp <k82.y12@gmail.com>
2022-07-26 21:20:38 -0700

    add the soluton of cyclic permutation for the assignment

--- Swift-AGDS/Graph/Cyclic Permutation.swift
+++ Swift-AGDS/Graph/Cyclic Permutation.swift
@@ -7,3 +7,47 @@
 
 import Foundation
 
+func cyclicPermutation() {
+    let testCase = Int(readLine()!)!
+    var caseNum = 0
+    
+    while caseNum < testCase {
+        
+        let n = Int(readLine()!)!
+        
+        let permutation = readLine()!.split(separator: " ")
+        
+        // adjacency list
+        var adjList = [[Int]](repeating: [], count: n + 1)
+        
+        for i in 0..<n {
+            let u = i + 1
+            let v = Int(permutation[u-1])!
+            adjList[u].append(v)
+        }
+        print("adjacency list ", adjList)
+        
+        func dfs(start: Int, adjList: inout [[Int]], visited: inout [Bool]) {
+            visited[start] = true
+            for v in adjList[start] {
+                if !visited[v] {
+                    dfs(start: v, adjList: &adjList, visited: &visited)
+                }
+            }
+        }
+        
+        var count = 0
+        var visited = [Bool](repeating: false, count: n + 1)
+        
+        for v in 1..<n {
+            if !visited[v] {
+                dfs(start: v, adjList: &adjList, visited: &visited)
+                count+=1
+            }
+        }
+        print(count)
+        
+        caseNum+=1
+    }
+}
+
--- Swift-AGDS/main.swift
+++ Swift-AGDS/main.swift
@@ -7,11 +7,16 @@
 
 import Foundation
 
+//Sample Input 1
 //2
 //8
 //3 2 7 8 1 4 5 6
 //10
 //2 1 3 4 5 6 7 9 10 8
+//Sample Output 1
+//3
+//7
+//
+//
 
-
-
+cyclicPermutation()
