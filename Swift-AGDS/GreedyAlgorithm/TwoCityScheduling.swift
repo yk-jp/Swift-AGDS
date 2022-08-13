4f469d73561df565f4ee9c8ed5027b83fb3467a5
yk-jp <k82.y12@gmail.com>
2022-08-12 21:39:35 -0700

    pass leetcode 1029

---
+++ Swift-AGDS/GreedyAlgorithm/TwoCityScheduling.swift
@@ -0,0 +1,36 @@
+//
+//  TwoCityScheduling.swift
+//  Swift-AGDS
+//
+//  Created by Yusuke K on 2022-08-12.
+//
+
+import Foundation
+
+func twoCitySchedCost(_ costs: [[Int]]) -> Int {
+    var diffArr :[[Int]] = [] // [idx, diff ]
+    
+    for (idx, cost) in costs.enumerated() {
+        let costA:Int = cost[0]
+        let costB:Int = cost[1]
+        
+        diffArr.append([idx, costA - costB])
+    }
+    
+    
+    diffArr = diffArr.sorted(by:{ $0[1] < $1[1] })
+    
+    var sum = 0
+    
+    for (idx, val) in diffArr.enumerated() {
+        let accessAt = val[0]
+        if idx < diffArr.count/2 {
+            sum += costs[accessAt][0]
+            continue
+        }
+        
+        sum += costs[accessAt][1]
+    }
+    
+    return sum
+}
